a045691e0ddee029b48fd9d78ff7056a5f803ec7
denniztr <denniztr@gmail.com>
2024-03-27 16:52:28 +0300

    fix

--- app/authorization/components/AuthForm.tsx
+++ app/authorization/components/AuthForm.tsx
@@ -4,9 +4,7 @@ import * as z from 'zod';
 
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-
 import { FormSchema } from '@/schemas';
-
 import {
   Form,
   FormControl,
