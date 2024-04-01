75e12fd1d3a30e68859a163f06cbcb6978753101
denniztr <denniztr@gmail.com>
2024-04-01 15:54:19 +0300

    shadcn form init

--- components/event/AddEventForm.tsx
+++ components/event/AddEventForm.tsx
@@ -1,13 +1,174 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { AddEventSchema } from '@/schemas';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+
 import { Input } from '../ui/input';
 import { Textarea } from '../ui/textarea';
+import { Label } from '../ui/label';
+import { Button } from '../ui/button';
+
 const AddEventForm = () => {
+  const form = useForm<z.infer<typeof AddEventSchema>>({
+    resolver: zodResolver(AddEventSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      location: '',
+      startDate: '',
+      startTime: '',
+      eventType: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof AddEventSchema>) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      Форма добавления
-      <form className='space-y-3'>
-        <Input className="w-full pb-" placeholder="Название мероприятия" />
-        <Textarea />
-      </form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <div className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <h3>Название мероприятия</h3>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="Название мероприятия"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <div>
+              <h3>Время начала мероприятия</h3>
+              <div className="flex space-x-2">
+                <FormField
+                  control={form.control}
+                  name="startDate"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input {...field} type="date" />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <div></div>
+                <FormField
+                  control={form.control}
+                  name="startTime"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input {...field} type="time" />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <h3>Место проведения</h3>
+                  <FormControl>
+                    <Input {...field} placeholder="Локация" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <h3>Описание мероприятия</h3>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      placeholder="Опишите подробно мероприятие"
+                      className="min-h-64"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="media"
+              render={({ field }) => (
+                <FormItem>
+                  <h3>Добавьте фотографию</h3>
+                  <FormControl>
+                    <Input {...field} type="file" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" variant="outline">
+            Опубликовать
+          </Button>
+        </form>
+      </Form>
+      {/* <form className="">
+          <Input className="w-full pb-" placeholder="Название мероприятия" />
+          <div>
+            Выберите вид мероприятия
+            <RadioGroup defaultValue="comfortable" className="flex gap-10">
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="offlineEvent" id="r1" />
+                <Label htmlFor="r1">Оффлайн мероприятие</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="onlineEvent" id="r2" />
+                <Label htmlFor="r2">Онлайн мероприятие</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <div>
+            <h3>Время начала мероприятия</h3>
+            <div className="flex space-x-2">
+              <Input type="date" className="w-36" />
+              <Input type="time" className="w-24" />
+            </div>
+          </div>
+          <div>
+            <h3>Описание мероприятия</h3>
+            <Textarea placeholder="Опишите подробно мероприятие" />
+          </div>
+          <div>
+            <h3>Выберите фотографию</h3>
+            <Input type="file" />
+          </div>
+          <Button type="submit" variant="outline">
+            Опубликовать
+          </Button>
+        </form> */}
     </div>
   );
 };
--- schemas/index.ts
+++ schemas/index.ts
@@ -4,24 +4,38 @@ import * as z from 'zod';
 
 export const SignUpSchema = z.object({
   email: z.string().email({
-    message: 'Введите электронную почту'
+    message: 'Введите электронную почту',
   }),
   name: z.string().min(1, {
-    message: 'Это поле обязятельное'
+    message: 'Это поле обязятельное',
   }),
   surname: z.string().min(1, {
-    message: 'Это поле обязательное'
+    message: 'Это поле обязательное',
   }),
   password: z.string().min(5, {
-    message: 'Пароль должен содержать минимум 5 символов'
+    message: 'Пароль должен содержать минимум 5 символов',
   }),
 });
 
 export const SignInSchema = z.object({
   email: z.string().min(1, {
-    message: 'Заполните поле'
+    message: 'Заполните поле',
   }),
   password: z.string().min(1, {
-    message: 'Заполните поле'
+    message: 'Заполните поле',
   }),
-})
+});
+
+export const AddEventSchema = z.object({
+  title: z.string().min(10, {
+    message: 'Поле должно содержать минимум 10 символов',
+  }),
+  description: z.string().min(10, {
+    message: 'Поле должно содержать минимум 10 символов',
+  }),
+  location: z.string(),
+  startDate: z.string(),
+  startTime: z.string(),
+  eventType: z.string(),
+  media: z.string(),
+});
